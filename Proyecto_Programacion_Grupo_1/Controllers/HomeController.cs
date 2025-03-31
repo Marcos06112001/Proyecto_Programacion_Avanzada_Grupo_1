7aa1f21aa8f0905f9c82752c8001a29c11451ab0
Marcos06112001 <134448696+Marcos06112001@users.noreply.github.com>
2025-03-31 15:29:47 -0600

    Cambios y Estructura
    Programacion

--- Proyecto_Programacion_Grupo_1/Controllers/HomeController.cs
+++ Proyecto_Programacion_Grupo_1/Controllers/HomeController.cs
@@ -2,30 +2,51 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Proyecto_Programacion_Grupo_1.Models;
 
-namespace Proyecto_Programacion_Grupo_1.Controllers;
-
-public class HomeController : Controller
+namespace Proyecto_Programacion_Grupo_1.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Inicio()
+        {
+            return View(); // Regresa la vista Inicio.cshtml
+        }
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
+        public IActionResult Userr()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
